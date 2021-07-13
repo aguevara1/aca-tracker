48a42c8d6f64d4e31184e23819faa3b6e79d8191
aguevara1 <albguevara12@gmail.com>
2021-07-13 13:28:43 -0500

    done with tracker app

--- public/tracker.js
+++ public/tracker.js
@@ -1,30 +1,25 @@
 
 let currentId=0;
-let clients=[];    // was called obj before
+let clients=[];   
+let temObject={}; 
 
 const data = {};
 let theName;
 let user=theName;
 let text="";
+let compareObjects=[];
 
 let lati=null;
 let longi=null;
 
 function renameUser(){
 
-  navigator.geolocation.getCurrentPosition(success, error, options);
      theName= $('#name').val();
    if(theName == null || theName == ''){
       theName="Guest";
    }
-   /*
-       if(lastClientId!=0){
-        if(clients.indexOf(lastClientId) !== -1){
-          alert("Value exists");
-       } else{  */
-     
+
           let nameOfUser={
-            
             name: theName
             };
 
@@ -33,19 +28,17 @@ function renameUser(){
           method: "POST",
           mode: 'cors',
           headers: {"Content-Type": "application/json"},
-          body: JSON.stringify(nameOfUser)
-         
+          body: JSON.stringify(nameOfUser)        
          })
          
-        //  .then(response => response.json())     
-         
-        
-           listUsers();
-        }
+        $('#prompt').fadeOut(200);
+}
+       
 
+setInterval(function(){ 
+//function listUsers(){
 
-
-function listUsers(){
+  navigator.geolocation.getCurrentPosition(success, error, options);
 //let messagesDivForUser = document.getElementById('who')
 //let displayUser = '';
 fetch('http://localhost:8000/clients', {
@@ -61,27 +54,28 @@ fetch('http://localhost:8000/clients', {
          userData.lat=lati;
          userData.long=longi;
          clients.push(userData);
-            currentId=userData.clientId;
-        console.log('this is what I got in userData:', userData);
-        console.log('this is in clients:', clients);
+         temObject=JSON.parse(JSON.stringify(userData));
 
+            currentId=userData.clientId;
+        console.log('This is the currentId',currentId);
+        
+        //userData has all the right information
+        console.log('this is what I got in userData:', userData);
+
+        console.log('This is the temObject',temObject);
+
+        //clients array also has right information
+        console.log('this is in clients:', clients);
       }) 
 
    postToLocations();  
-}
+}, 1000)
 
-
-
-
- // navigator.geolocation.getCurrentPosition(success, error, options);
-
+ 
   function success(pos) {
-    var crd = pos.coords;
-      
+    var crd = pos.coords;   
     lati=crd.latitude;
     longi=crd.longitude;
-
-
   }
 
   function error(err) {
@@ -94,28 +88,63 @@ fetch('http://localhost:8000/clients', {
     maximumAge: 0
   };
 
-
-
   function postToLocations(){
-
       let tempObj={};
-       tempObj=clients[currentId-1];
-    //  tempObj.clientId=clients[currentId-1].clientId;
-     // tempObj.lat=clients[currentId-1].lat;
-     // tempObj.long=clients[currentId-1].long;
+      
+      //clients doesn't have clientId or name, they are null, this part not working
+       tempObj=clients[currentId];
+     //  console.log('before doing post to locations', clients[currentId+5]);
+       
 
     fetch('http://localhost:8000/locations', {
       // fetch('/clients', {
          method: "POST",
          mode: 'cors',
          headers: {"Content-Type": "application/json"},
-         body: JSON.stringify(tempObj)
-        
+         body: JSON.stringify(temObject) 
         })
-
-       
-
+            // displayMessages();
   }
 
+  // Working on this to display on webpage users with username,lat,long
+  //location
+  
+   // function displayMessages(){
+    setInterval(function(){ 
+    let messagesDiv = document.getElementById('messagesForDisplay')
+    let displayMsg = '';
+   // fetch('http://localhost:8080/messages')
+    fetch('http://localhost:8000/locations',{
+    method: "GET",
+    mode: 'cors', 
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })  
 
+        .then(response => response.json())
+        //need to loop through m and just display the ones that are different
+        .then(data => { 
+           console.log('data after GET', data);
+             data.map(m => {
+                  if(m.name){
+                  //  compareObjects.push(m);
+
+
+              //  var my_obj_str = JSON.stringify(m.location});
+                // if(JSON.stringify(m.location))
+                  displayMsg += `<div>${m.name}&nbsp: ${JSON.stringify(m.location)}</div><br>`
+                     
+                }
+              })
+  
+             var newMessage= displayMsg.replace('<div>Guest&nbsp: Joined</div><br>', ''); 
+            
+           messagesDiv.innerHTML = newMessage;
+          })   
+          // console.log('data after GET', data);
+
+  }, 1000)
+  
+  
   
--- routes/location.js
+++ routes/location.js
@@ -3,47 +3,78 @@ const app=express();
 const router=express.Router();
 let fetch = require('node-fetch');
 let cors= require('cors');
-
-app.use(cors());
+//app.use(cors());
+let count=0;
+let y=0;
+let uniqueObjects=[];
 
 let latit=null;
 let longit=null;
 let obj={};
 let anotherObj={};
+let arrayOfObjects=[]; //put all user objects in here
+let userInfo={};
 
   router.post('/locations', (request, response)=>{
 
-      let location={}
-      location=request.body;
+      userInfo=request.body;
+    //  console.log('this is the body of post to locations',userInfo);
 
       latit=Number(request.body.lat);
       longit=Number(request.body.long);
    
-      fetch('http://nominatim.openstreetmap.org/reverse?format=json&lat=${latit}&lon=${longit}&zoom=18&addressdetails=1', 
+      fetch(`http://nominatim.openstreetmap.org/reverse?format=json&lat=${latit}&lon=${longit}&zoom=18&addressdetails=1`, 
       { 
-        method:'POST', 
+        method:'GET', 
         //mode: 'no-cors',
-        headers: 
-          {"Content-Type": "application/json"}
+        headers:
+        {  "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"} 
+         // {"Content-Type": "application/json"}
          // body: JSON.stringify()
       })
-      .then(response => response.json())
+      .then(response => {
+      //  console.log(response.status);
+        return response.json();
+      })
       .then(data => {
-        console.log(typeof data);
-        anotherObj=JSON.stringify(data);
-        
-       obj.lat = anotherObj.lat;
-        obj.long =anotherObj.lon;
-        obj.location =anotherObj.address;
-       // response.send(obj);
-        console.log(obj);
+      //  console.log(typeof data);
+       // anotherObj=JSON.parse(data);
+        //  console.log('this is the data', data);
+        obj.lat =data.lat;
+        obj.long =data.lon;
+        obj.location =data.address;
+
+        // ****** userinfo has clientId, name, lat and long already, just adding address 
+        userInfo.location=data.address;
+   
+         // adding objects to Array to send back
+        arrayOfObjects.push(userInfo);
+           count=arrayOfObjects.length;
+           console.log('this is the arrayOfObjects count', count);
+           
+       // console.log('this is arrayOfObjects',arrayOfObjects);
+     
       });
-      
+    
+           // Somewhat working
+             uniqueObjects = [...new Map(arrayOfObjects.map(item => [item.name, item])).values()]
+     
+
+
+       console.log('this is the uniqueObjects count', uniqueObjects.length);
+     console.log('these are the objects in uniqueObjects', uniqueObjects);
+        
+                  
+        response.json(uniqueObjects);
   }) 
 
 
+
 router.get('/locations', (request, response) =>{
- 
+
+     return response.json(uniqueObjects);
+
  })
+
 
 module.exports=router;
